2ecf46f10b790d0b99ee0f83c7e74220eb366b01
Jacob <jacobshorterivey@gmail.com>
2019-09-14 14:47:47 -0600

    objective complete

--- README.md
+++ README.md
@@ -4,9 +4,14 @@
 
 ### Overview
 
-This program simulates a cash transaction interface.
+This program simulates a cash transaction interface. It calculates change and returns it in correct denominations.
 ### How to Run
+
+Compile MakeChangeApp.java, then run MakeChangeApp.
 
 ### Technologies/Topics Used
 
+The modulus (%) operator to properly calculate the remaining amount of change, if-else statements to provide conditions, and casting doubles as ints to round off amounts smaller than $0.01.
+
 ### Lessons
+Variables, operators, if-else statements, user input, and casting variables.
--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -4,15 +4,7 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-
-		int twentyBills = 0;
-		int tenBills = 0;
-		int fiveBills = 0;
-		int singleBills = 0;
-		int quarters = 0;
-		int dimes = 0;
-		int nickels = 0;
-		int pennies = 0;
+		int twentyBills, tenBills, fiveBills, singleBills, quarters, dimes, nickels, pennies;
 
 		System.out.print("Enter an item price: ");
 		double price = kb.nextDouble();
@@ -20,76 +12,73 @@ public class MakeChangeApp {
 		double tender = kb.nextDouble();
 
 		if (tender < price) {
-			System.out.println("Too little money tendered.");
+			System.out.println("Too little money tendered."); // not enough money
 		} else if (tender == price) {
-			System.out.println("Exact amount given.");
+			System.out.println("Exact amount given. No change."); // exact amount; no change needed
 		} else {
-			System.out.println("Change is $" + (tender - price));
-			pennies = (int) ((tender - price + 0.000001) * 100);
-			System.out.println("That is " + pennies + " pennies.");
-			
-			// does not pass Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
+			pennies = (int) ((tender - price + 0.000001) * 100); // multiply by 100 and cast as int. addition corrects
+																	// floating point
 
-			if (pennies >= 2000) {
-				twentyBills = pennies / 2000;
+			if (pennies >= 2000) { // if there are over 2000 pennies, we owe them a twenty
+				twentyBills = pennies / 2000; // give them the appropriate number of twenties
 				if (twentyBills > 1) {
-					System.out.print(twentyBills + "twenties, ");
+					System.out.print(twentyBills + "twenties. ");
 				} else if (twentyBills == 1) {
-					System.out.print(twentyBills + "twenty, ");
+					System.out.print(twentyBills + "twenty. ");
 				}
-				pennies = pennies % 2000;
+				pennies = pennies % 2000; // the amount of remaining pennies after taking out the twenties
 			}
 			if (pennies >= 1000) {
 				tenBills = pennies / 1000;
 				if (tenBills > 1) {
-					System.out.print(tenBills + " tens, ");
+					System.out.print(tenBills + " tens. ");
 				} else if (tenBills == 1) {
-					System.out.print(tenBills + " ten, ");
+					System.out.print(tenBills + " ten. ");
 				}
 				pennies = pennies % 1000;
 			}
 			if (pennies >= 500) {
 				fiveBills = pennies / 500;
 				if (fiveBills > 1) {
-					System.out.print(fiveBills + " fives, ");
+					System.out.print(fiveBills + " fives. ");
 				} else if (fiveBills == 1) {
-					System.out.print(fiveBills + " five, ");
+					System.out.print(fiveBills + " five. ");
 				}
 				pennies = pennies % 500;
 			}
 			if (pennies >= 100) {
 				singleBills = pennies / 100;
 				if (singleBills > 1) {
-					System.out.print(singleBills + " ones, ");
+					System.out.print(singleBills + " ones. ");
 				} else if (singleBills == 1) {
-					System.out.print(singleBills + " one, ");
+					System.out.print(singleBills + " one. ");
 				}
 				pennies = pennies % 100;
 			}
 			if (pennies >= 25) {
 				quarters = pennies / 25;
 				if (quarters > 1) {
-					System.out.print(quarters + " quarters, ");
+					System.out.print(quarters + " quarters. ");
 				} else if (quarters == 1) {
-					System.out.print(quarters + " quarter, ");
+					System.out.print(quarters + " quarter. ");
 				}
 				pennies = pennies % 25;
 			}
 			if (pennies >= 10) {
 				dimes = pennies / 10;
 				if (dimes > 1) {
-					System.out.print(dimes + " dimes, ");
+					System.out.print(dimes + " dimes. ");
 				} else if (dimes == 1) {
-					System.out.print(dimes + " dime, ");
+					System.out.print(dimes + " dime. ");
 				}
 				pennies = pennies % 10;
 			}
 			if (pennies >= 5) {
 				nickels = pennies / 5;
 				if (nickels > 1) {
-					System.out.print(nickels + " nickels, ");
+					System.out.print(nickels + " nickels. ");
 				} else if (nickels == 1) {
-					System.out.print(nickels + " nickel, ");
+					System.out.print(nickels + " nickel. ");
 				}
 				pennies = pennies % 5;
 			}
@@ -102,5 +91,7 @@ public class MakeChangeApp {
 		}
 		kb.close();
 	}
+	
+	//add some methods for formatting?
 
 }
